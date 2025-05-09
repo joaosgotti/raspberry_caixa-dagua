476e34139e534237e2163cd3532a6f944e77050d
João Vítor Sgotti Veiga <jvsgottiveiga@gmail.com>
2025-05-09 15:05:26 -0300

    anotehr debug

--- models.py
+++ models.py
@@ -9,11 +9,9 @@ import os
 from dotenv import load_dotenv
 
 print("[Models] Carregando variáveis de ambiente do arquivo .env...")
-print(os.getenv("DB_USER"))
+USER = os.getenv("DB_USER")
+print(f"oiee, cheguei aqui, USER: {USER}")
 DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
-
-
-
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
