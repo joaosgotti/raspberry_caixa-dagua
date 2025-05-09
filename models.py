00ab575adf9efb83ece8d21855e946fc9e5d35f4
João Vítor Sgotti Veiga <jvsgottiveiga@gmail.com>
2025-05-09 10:20:31 -0300

    aspas simples na database_url

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import SQLAlchemyError # Para capturar erros do SQLAlchemy
 import os
 
-DATABASE_URL = f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
+DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
