915ebb16379a312c9571aa16ad9251e24c08a4e1
João Vítor Sgotti Veiga <jvsgottiveiga@gmail.com>
2025-05-09 16:38:32 -0300

    fix: tirei a verificacao de validade da mediana

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ SETTLE_TIME_S = float(os.getenv("SETTLE_TIME_S"))
 MAX_ECHO_WAIT_S = float(os.getenv("MAX_ECHO_WAIT_S"))
 MAX_VALID_PULSE_S = float(os.getenv("MAX_VALID_PULSE_S"))
 MIN_VALID_PULSE_S = float(os.getenv("MIN_VALID_PULSE_S"))
-TOLERANCIA = float(os.getenv("TOLERANCIA")) 
+
 
 def run_publisher_with_sensor():
     """
@@ -46,14 +46,10 @@ def run_publisher_with_sensor():
             # Lê a MEDIANA da distância
             median_distance_value = sensor_reader.get_median_distance()
             if median_distance_value is not None:
-                # Validação básica da MEDIANA lida
-                if (MIN_NIVEL*(1-TOLERANCIA)) < median_distance_value < (MAX_NIVEL*(1+TOLERANCIA)):
-                    print(f"Mediana do sensor: {median_distance_value} cm (Válida)")
+                created_on = datetime.now().isoformat() # Usa o timezone UTC e formato ISO
+                print(f"Mediana do sensor: {median_distance_value} cm | created_on: {created_on}")
+                DatabaseHandler().insert_reading(median_distance_value, created_on)
 
-                    created_on = datetime.now().isoformat() # Usa o timezone UTC e formato ISO
-                    DatabaseHandler().insert_reading(median_distance_value, created_on)
-                else:
-                    print(f"Mediana do sensor: {median_distance_value:.1f} cm (Fora da faixa esperada, ignorando)")
             else:
                 print("[Publisher Main] Falha ao obter a mediana do sensor. Pulando a publicação.")
 
