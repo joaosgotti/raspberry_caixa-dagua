525cb65feeb8a2d2839bcff9518da0e754467c55
João Vítor Sgotti Veiga <jvsgottiveiga@gmail.com>
2025-05-09 14:57:47 -0300

    aspas no DATABASE_URL

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine("DATABASE_URL")
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
